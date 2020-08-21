77908ae6a9715e7f6d9338b32edf828937af77cc
Fernando Costa <ferco0@live.com>
2020-08-21 11:39:50 -0300

    Add typescrypt definition file (#18)
    * Add typescrypt definition file
    
    * Add types reference to package.json
    
    * Fix "resetTimeout" options description
    
    * Apply suggestions from code review
    
    Co-authored-by: Maksim Sinik <maksim@sinik.it>
    
    * fix import type name
    
    add type commonjs to packages.json
    
    * fix beforeHandler return type
    
    * add tests and dependencies
    
    * add all supported OS to the CI matrix
    
    * fix ci.yml file
    
    * Update package.json
    
    Co-authored-by: Manuel Spigolon <behemoth89@gmail.com>
    
    * Update ci.yml
    
    Co-authored-by: Maksim Sinik <maksim@sinik.it>
    Co-authored-by: Manuel Spigolon <behemoth89@gmail.com>

---
+++ index.d.ts
@@ -0,0 +1,45 @@
+import {
+  FastifyPlugin,
+  FastifyRequest,
+  FastifyReply,
+  HookHandlerDoneFunction,
+} from "fastify";
+
+declare module "fastify" {
+  interface FastifyInstance {
+    circuitBreaker(
+      options?: FastifyCircuitBreakerOptions
+    ): FastifyCircuitBreakerBeforeHandler;
+  }
+}
+
+export interface FastifyCircuitBreakerBeforeHandler {
+  (
+    req: FastifyRequest,
+    reply: FastifyReply,
+    next: HookHandlerDoneFunction
+  ): Promise<unknown> | void;
+}
+
+export type FastifyCircuitBreakerOptions = {
+  /**
+   * The maximum numbers of failures you accept to have before opening the circuit.
+   * @default 5
+   */
+  threshold?: number;
+  /**
+   * The maximum number of milliseconds you can wait before return a `TimeoutError`.
+   * @default 10000
+   */
+  timeout?: number;
+
+  /**
+   * The number of milliseconds before the circuit will move from `open` to `half-open`.
+   * @default 10000
+   */
+  resetTimeout?: number;
+};
+
+export const fastifyCircuitBreaker: FastifyPlugin<FastifyCircuitBreakerOptions>;
+
+export default fastifyCircuitBreaker;
