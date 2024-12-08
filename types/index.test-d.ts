bc40b9d338a63ad59da85fa83964f0ca58674cb8
Frazer Smith <frazer.dev@icloud.com>
2024-12-08 13:01:54 +0000

    build(deps-dev): replace standard with neostandard (#138)

--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -1,36 +1,36 @@
-import fastify, { FastifyReply, FastifyRequest } from "fastify";
-import { expectType } from "tsd";
-import FastifyCircuitBreaker, { FastifyCircuitBreakerOptions } from "..";
+import fastify, { FastifyReply, FastifyRequest } from 'fastify'
+import { expectType } from 'tsd'
+import FastifyCircuitBreaker, { FastifyCircuitBreakerOptions } from '..'
 
-const app = fastify();
+const app = fastify()
 
-app.register(FastifyCircuitBreaker);
-app.register(FastifyCircuitBreaker, {});
-app.register(FastifyCircuitBreaker, { timeout: 5000 });
-app.register(FastifyCircuitBreaker, { threshold: 5 });
-app.register(FastifyCircuitBreaker, { resetTimeout: 10000 });
+app.register(FastifyCircuitBreaker)
+app.register(FastifyCircuitBreaker, {})
+app.register(FastifyCircuitBreaker, { timeout: 5000 })
+app.register(FastifyCircuitBreaker, { threshold: 5 })
+app.register(FastifyCircuitBreaker, { resetTimeout: 10000 })
 app.register(FastifyCircuitBreaker, {
   timeout: 5000,
   threshold: 5,
   resetTimeout: 10000,
-});
+})
 
 const fastifyCircuitBreakerOptions: FastifyCircuitBreakerOptions = {
   timeout: 5000,
   threshold: 5,
   resetTimeout: 10000,
-};
-app.register(FastifyCircuitBreaker, fastifyCircuitBreakerOptions);
+}
+app.register(FastifyCircuitBreaker, fastifyCircuitBreakerOptions)
 
 app.get(
-  "/",
+  '/',
   {
     preHandler: app.circuitBreaker(),
   },
   (req, reply) => { }
-);
+)
 
-app.register(FastifyCircuitBreaker, { timeoutErrorMessage: 'Timeon' });
+app.register(FastifyCircuitBreaker, { timeoutErrorMessage: 'Timeon' })
 app.register(FastifyCircuitBreaker, {
   onTimeout: async (req, reply) => {
     expectType<FastifyRequest>(req)
@@ -39,7 +39,7 @@ app.register(FastifyCircuitBreaker, {
     reply.statusCode = statusCode
     throw new Error('timed out')
   }
-});
+})
 app.register(FastifyCircuitBreaker, {
   onTimeout: (req, reply) => {
     expectType<FastifyRequest>(req)
@@ -47,7 +47,7 @@ app.register(FastifyCircuitBreaker, {
     reply.statusCode = 504
     return 'timed out'
   }
-});
+})
 app.register(FastifyCircuitBreaker, {
   onTimeout: async (req, reply) => {
     expectType<FastifyRequest>(req)
@@ -55,9 +55,9 @@ app.register(FastifyCircuitBreaker, {
     reply.statusCode = 504
     return 'timed out'
   }
-});
+})
 
-app.register(FastifyCircuitBreaker, { circuitOpenErrorMessage: 'circus open' });
+app.register(FastifyCircuitBreaker, { circuitOpenErrorMessage: 'circus open' })
 app.register(FastifyCircuitBreaker, {
   onCircuitOpen: async (req, reply) => {
     expectType<FastifyRequest>(req)
@@ -66,7 +66,7 @@ app.register(FastifyCircuitBreaker, {
     reply.statusCode = statusCode
     throw new Error('circuit open')
   }
-});
+})
 app.register(FastifyCircuitBreaker, {
   onCircuitOpen: (req, reply) => {
     expectType<FastifyRequest>(req)
@@ -74,7 +74,7 @@ app.register(FastifyCircuitBreaker, {
     reply.statusCode = 504
     return 'circuit open'
   }
-});
+})
 app.register(FastifyCircuitBreaker, {
   onCircuitOpen: async (req, reply) => {
     expectType<FastifyRequest>(req)
@@ -82,4 +82,4 @@ app.register(FastifyCircuitBreaker, {
     reply.statusCode = 504
     return 'circuit open'
   }
-});
+})
